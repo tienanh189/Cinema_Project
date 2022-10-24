e3b261d793819fcb0c8ef2da2b4322e53c80022e
tienanh189 <letienanh1892001@gmail.com>
2022-10-24 20:19:44 +0700

    24/10: Update database

--- CinemaAPI/Models/CinemaDbContext.cs
+++ CinemaAPI/Models/CinemaDbContext.cs
@@ -4,8 +4,29 @@ namespace CinemaAPI.Models
 {
     public class CinemaDbContext : DbContext
     {
+        public CinemaDbContext()
+        {
+        }
+
         public CinemaDbContext(DbContextOptions<CinemaDbContext> options) : base(options)
         {
         }
+
+        public DbSet<Bill> Bill { get; set; }
+        public DbSet<CategoryMovie> CategoryMovie { get; set; } 
+        public DbSet<CategoryMovie_Movie> CategoryMovie_Movie { get; set; }
+        public DbSet<CategorySeat> CategorySeat { get; set; }
+        public DbSet<Discount> Discount { get; set; }
+        public DbSet<Cinema> Cinema { get; set; }
+        public DbSet<Movie> Movie { get; set; }
+        public DbSet<Permission> Permission { get; set; }
+        public DbSet<Room> Room { get; set; }
+        public DbSet<Seat> Seat { get; set; }
+        public DbSet<ShowTime> ShowTime { get; set; }
+        public DbSet<ShowTime_Seat> ShowTime_Seat { get; set; }
+        public DbSet<Ticket> Ticket { get; set; }
+        public DbSet<User> User { get; set; }
+        public DbSet<UserGroup> UserGroup {get; set; }
+        public DbSet<UserGroup_Permission> UserGroup_Permission { get; set; }
     }
 }
--- CinemaAPI/Models/UserGroup.cs
+++ CinemaAPI/Models/UserGroup.cs
@@ -5,6 +5,6 @@
         public Guid UserGroupId { get; set; }
         public string? UserGroupName { get; set; }
         public ICollection<UserGroup_Permission> UserGroup_Permissions { get; set; }
-
+        public ICollection<User> Users { get; set; }
     }
 }
