6610635b2f894c04ad324b1b36af7f3fae3bae0f
tienanh189 <letienanh1892001@gmail.com>
2022-10-29 12:32:12 +0700

    29/10: Fix bug CORS Policy

--- CinemaAPI/Program.cs
+++ CinemaAPI/Program.cs
@@ -28,19 +28,23 @@ builder.Services.AddScoped<IDiscountRespository, DiscountRespository>();
 builder.Services.AddScoped<IBillRespository, BillRespository>();
 builder.Services.AddScoped<ICategoryMovie_MovieRespository, CategoryMovie_MovieRespository>();
 builder.Services.AddScoped<IShowTime_SeatRespository, ShowTime_SeatRespository>();
-
 builder.Services.AddScoped<IRoomRespository, RoomRespository>();
-
 builder.Services.AddScoped<ICinemaRespository, CinemaRespository>();
-
 builder.Services.AddScoped<IMovieRespository, MovieRespository>();
-
 builder.Services.AddScoped<IShowTimeRespository, ShowTimeRespository>();
-
 builder.Services.AddScoped<ITicketRespository, TicketRespository>();
 
 
-
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      builder =>
+                      {
+                          builder.WithOrigins("http://example.com",
+                                              "http://www.contoso.com");
+                      });
+});
 
 
 var app = builder.Build();
@@ -53,9 +57,10 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.UseStaticFiles();
+app.UseRouting();
+app.UseCors(MyAllowSpecificOrigins);
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
